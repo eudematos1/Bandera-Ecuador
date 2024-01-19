035189fc71f12eebb7ffeccab88f089196f06980
eudematos1 <116099820+eudematos1@users.noreply.github.com>
2024-01-19 08:48:22 -0500

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// App.js
+import React, { useState } from 'react';
+import { View, StyleSheet, TouchableOpacity, Text } from 'react-native';
 
-export default function App() {
+const App = () => {
+  const [mensaje, setMensaje] = useState('');
+
+  const mostrarMensajeAmarillo = () => {
+    setMensaje('Amarillo - Ecuador');
+  };
+
+  const mostrarMensajeAzul = () => {
+    setMensaje('Azul - Ecuador');
+  };
+
+  const mostrarMensajeRojo = () => {
+    setMensaje('Rojo - Ecuador');
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.cuadradoAmarillo}>
+      <TouchableOpacity onPress={mostrarMensajeAmarillo} style={styles.cuadradoAmarillo}>
+        <View style={styles.cuadradoAzul}>
+          <TouchableOpacity onPress={mostrarMensajeAzul} style={styles.cuadradoAzul}>
+            <TouchableOpacity onPress={mostrarMensajeRojo} style={styles.cuadradoRojo}></TouchableOpacity>
+          </TouchableOpacity>
+        </View>
+      </TouchableOpacity>
+      {mensaje !== '' && <Text>{mensaje}</Text>}
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+  cuadradoAmarillo: {
+    width: 200,
+    height: 200,
+    backgroundColor: 'yellow',
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  cuadradoAzul: {
+    width: 150,
+    height: 150,
+    backgroundColor: 'blue',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  cuadradoRojo: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'red',
   },
 });
+
+export default App;
